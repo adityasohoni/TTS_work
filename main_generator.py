ae50f955446fb2244e4f5bcd27faad4cb4f3483e
adityasohoni <adityasohoni@gmail.com>
2022-08-04 23:43:05 +0530

    Modified metadata.csv again

--- main_generator.py
+++ main_generator.py
@@ -15,10 +15,10 @@ from rich.table import Table
 import wave
 # from datetime import datetime
 # import librosa
-import struct
-import numpy as np
-import webrtcvad
-from scipy.ndimage.morphology import binary_dilation
+# import struct
+# import numpy as np
+# import webrtcvad
+# from scipy.ndimage.morphology import binary_dilation
 # import soundfile
 import sys
 import csv
@@ -30,48 +30,48 @@ channels = 1
 frame_rate = 22050
 timeout = 60
 
-# For silence trimming
-vad_moving_average_width = 8
-int16_max = (2 ** 15) - 1
-vad_window_length = 30
-sample_rate = 16000
-vad_max_silence_length = 6
+# # For silence trimming
+# vad_moving_average_width = 8
+# int16_max = (2 ** 15) - 1
+# vad_window_length = 30
+# sample_rate = 16000
+# vad_max_silence_length = 6
 
 
-# Source: CorentinJ - Real Time Voice Cloning - Thanks for this one!
-def trim_long_silences(wav):
-    # Compute the voice detection window size
-    samples_per_window = (vad_window_length * sample_rate) // 1000
-
-    # Trim the end of the audio to have a multiple of the window size
-    wav = wav[:len(wav) - (len(wav) % samples_per_window)]
-
-    # Convert the float waveform to 16-bit mono PCM
-    pcm_wave = struct.pack("%dh" % len(wav), *(np.round(wav * int16_max)).astype(np.int16))
-
-    # Perform voice activation detection
-    voice_flags = []
-    vad = webrtcvad.Vad(mode=3)
-    for window_start in range(0, len(wav), samples_per_window):
-        window_end = window_start + samples_per_window
-        voice_flags.append(vad.is_speech(pcm_wave[window_start * 2:window_end * 2], sample_rate=sample_rate))
-    voice_flags = np.array(voice_flags)
-
-    # Smooth the voice detection with a moving average
-    def moving_average(array, width):
-        array_padded = np.concatenate((np.zeros((width - 1) // 2), array, np.zeros(width // 2)))
-        ret = np.cumsum(array_padded, dtype=float)
-        ret[width:] = ret[width:] - ret[:-width]
-        return ret[width - 1:] / width
-
-    audio_mask = moving_average(voice_flags, vad_moving_average_width)
-    audio_mask = np.round(audio_mask).astype(np.bool)
-
-    # Dilate the voiced regions
-    audio_mask = binary_dilation(audio_mask, np.ones(vad_max_silence_length + 1))
-    audio_mask = np.repeat(audio_mask, samples_per_window)
-
-    return wav[audio_mask == True]
+# # Source: CorentinJ - Real Time Voice Cloning - Thanks for this one!
+# def trim_long_silences(wav):
+#     # Compute the voice detection window size
+#     samples_per_window = (vad_window_length * sample_rate) // 1000
+#
+#     # Trim the end of the audio to have a multiple of the window size
+#     wav = wav[:len(wav) - (len(wav) % samples_per_window)]
+#
+#     # Convert the float waveform to 16-bit mono PCM
+#     pcm_wave = struct.pack("%dh" % len(wav), *(np.round(wav * int16_max)).astype(np.int16))
+#
+#     # Perform voice activation detection
+#     voice_flags = []
+#     vad = webrtcvad.Vad(mode=3)
+#     for window_start in range(0, len(wav), samples_per_window):
+#         window_end = window_start + samples_per_window
+#         voice_flags.append(vad.is_speech(pcm_wave[window_start * 2:window_end * 2], sample_rate=sample_rate))
+#     voice_flags = np.array(voice_flags)
+#
+#     # Smooth the voice detection with a moving average
+#     def moving_average(array, width):
+#         array_padded = np.concatenate((np.zeros((width - 1) // 2), array, np.zeros(width // 2)))
+#         ret = np.cumsum(array_padded, dtype=float)
+#         ret[width:] = ret[width:] - ret[:-width]
+#         return ret[width - 1:] / width
+#
+#     audio_mask = moving_average(voice_flags, vad_moving_average_width)
+#     audio_mask = np.round(audio_mask).astype(np.bool)
+#
+#     # Dilate the voiced regions
+#     audio_mask = binary_dilation(audio_mask, np.ones(vad_max_silence_length + 1))
+#     audio_mask = np.repeat(audio_mask, samples_per_window)
+#
+#     return wav[audio_mask == True]
 
 
 if __name__ == '__main__':
